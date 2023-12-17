0658dcce8c9fabcf3c0034e1d5783ee1a84014f2
yuuuu0311 <a26430803@gmail.com>
2023-12-17 16:58:24 +0800

    add test view

--- @shyu/vue-project/src/router/index.js
+++ @shyu/vue-project/src/router/index.js
@@ -1,23 +1,31 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+//  前端路由，用來做SPA的切換頁面
+
+import { createRouter, createWebHistory } from "vue-router";
+import TestView from "../views/TestView.vue";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/",
+            name: "test",
+            component: TestView,
+        },
+        // {
+        //     path: "/",
+        //     name: "home",
+        //     component: HomeView,
+        // },
+        // {
+        //     path: "/about",
+        //     name: "about",
+        //     // route level code-splitting
+        //     // this generates a separate chunk (About.[hash].js) for this route
+        //     // which is lazy-loaded when the route is visited.
+        //     component: () => import("../views/AboutView.vue"),
+        // },
+    ],
+});
 
-export default router
+export default router;
---
+++ @shyu/vue-project/src/views/TestView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import TheTest from "../components/TheTest.vue";
+// import TheWelcome from "../components/TheWelcome.vue";
+</script>
+
+<template>
+    <main>
+        <TheTest />
+    </main>
+</template>
+
+<style scoped>
+main {
+    height: min(100%, 100vh);
+}
+</style>
